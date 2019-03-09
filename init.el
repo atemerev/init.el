9e27be0eabe536dfdc7291f4bc5df2f6de9ff888
Alexander Temerev <sorhed@gmail.com>
2019-03-09 15:48:17 +0100

    Initial

---
+++ init.el
@@ -0,0 +1,37 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(setf inhibit-splash-screen t)
+(switch-to-buffer (get-buffer-create "empty"))
+(delete-other-windows)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(defun display-startup-echo-area-message ()
+  (message ""))
+(global-display-line-numbers-mode)
+(setq create-lockfiles nil)
+
+(if (display-graphic-p)
+    (progn
+      (setq initial-frame-alist
+            '(
+              (tool-bar-lines . 0)
+              (width . 106) ; chars
+              (height . 60) ; lines
+              (left . 50)
+              (top . 50)))
+      (setq default-frame-alist
+            '(
+              (tool-bar-lines . 0)
+              (width . 106)
+              (height . 60)
+              (left . 50)
+              (top . 50))))
+  (progn
+    (setq initial-frame-alist '( (tool-bar-lines . 0)))
+    (setq default-frame-alist '( (tool-bar-lines . 0)))))
